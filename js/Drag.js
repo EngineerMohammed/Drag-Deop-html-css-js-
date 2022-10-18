fd7c959d2c7b8f6e333dff1be7dd5f9bdbfee6f3
EngineerMohammed <eng_abo_hussien@yahoo.com>
2022-10-18 10:31:48 +0200

    Drag with javascript

---
+++ js/Drag.js
@@ -0,0 +1,49 @@
+let inp = document.getElementById("inp");
+let btn = document.getElementById("btn");
+let boxs = document.querySelectorAll(".box");
+let drag = null;
+
+btn.onclick = function () {
+  if (inp.value == "" || inp.value == null) {
+    alert("Your Input is empty");
+  } else {
+    let pargh = document.createElement("p");
+    pargh.className = "item";
+    let textP = document.createTextNode(inp.value);
+    pargh.setAttribute("draggable", "true");
+    pargh.appendChild(textP);
+    boxs[0].appendChild(pargh);
+    inp.value = "";
+  }
+  dragitem();
+};
+
+function dragitem() {
+  let items = document.querySelectorAll(".item");
+  items.forEach((item) => {
+    item.addEventListener("dragstart", function () {
+      drag = item;
+      item.style.opacity = "0.5";
+    });
+    item.addEventListener("dragend", function () {
+      drag = null;
+      item.style.opacity = "1";
+    });
+    boxs.forEach((box) => {
+      box.addEventListener("dragover", function (e) {
+        e.preventDefault();
+        this.style.background = "#090";
+        this.style.color = "#fff";
+      });
+      box.addEventListener("dragleave", function () {
+        this.style.background = "#fff";
+        this.style.color = "#000";
+      });
+      box.addEventListener("drop", function () {
+        box.append(drag);
+        this.style.background = "#fff";
+        this.style.color = "#000";
+      });
+    });
+  });
+}
